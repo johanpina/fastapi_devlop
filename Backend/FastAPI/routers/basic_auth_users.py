7506083a7b46cdcd9bd1c512f5706504f356b2ca
Johan S. Pina <johanpina1420@gmail.com>
2023-01-22 23:33:27 -0500

    Fixed problems with login method

--- Backend/FastAPI/routers/basic_auth_users.py
+++ Backend/FastAPI/routers/basic_auth_users.py
@@ -1,19 +1,20 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 # OAuth2PasswordBearer gestina el usuario y la contraseña
 # OAuth2PasswordRequestForm la forma en la que el backend envia desde el cliente el usr y la contraseña
 
+router = APIRouter()
 
-app = FastAPI()
+oauth2 = OAuth2PasswordBearer(tokenUrl="login")
 
-oauth2 = OAuth2PasswordBearer(tokenUrl='login') ## esta autenticación es más simple que la de jwt
 
 class User(BaseModel):
     username: str
     full_name: str
     email: str
-    disable: bool
+    disabled: bool
+
 
 class UserDB(User):
     password: str
@@ -22,58 +23,62 @@ class UserDB(User):
 users_db = {
     "mouredev": {
         "username": "mouredev",
-        "full_name": "Brais_moure",
+        "full_name": "Brais Moure",
         "email": "braismoure@mourede.com",
-        "disable": False,
+        "disabled": False,
         "password": "123456"
     },
-    "moredev2": {
+    "mouredev2": {
         "username": "mouredev2",
-        "full_name": "Brais_moure",
-        "email": "braismoure@mourede.com",
-        "disable": True,
-        "password": "123456"
+        "full_name": "Brais Moure 2",
+        "email": "braismoure2@mourede.com",
+        "disabled": True,
+        "password": "654321"
     }
 }
 
 
 def search_user_db(username: str):
     if username in users_db:
-        return(UserDB(**users_db[username]))
+        return UserDB(**users_db[username])
+
 
 def search_user(username: str):
     if username in users_db:
-        return(User(**users_db[username]))
+        return User(**users_db[username])
+
 
 async def current_user(token: str = Depends(oauth2)):
-    user =  search_user(token)
-    if not user: 
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail="Credenciales de atenticacion invalidas", 
-                            headers={"www-Authenticate": "Bearer"})
-    
-    if user.disable:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail="Usuario inactivo", 
-                            )
+    user = search_user(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Credenciales de autenticación inválidas",
+            headers={"WWW-Authenticate": "Bearer"})
+
+    if user.disabled:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Usuario inactivo")
 
     return user
 
-@app.post('/login')
-async def login(form: OAuth2PasswordRequestForm = Depends()): # depends dice que recibe datos pero no depende de nadie
+
+@router.post("/login")
+async def login(form: OAuth2PasswordRequestForm = Depends()):
     user_db = users_db.get(form.username)
     if not user_db:
-        raise HTTPException(status_code=400,detail="El usuario no es correcto")
-    
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="El usuario no es correcto")
+
     user = search_user_db(form.username)
-
     if not form.password == user.password:
-        raise HTTPException(status_code=400,detail="La contraseña no es correcta")
-    
-    return {"access_token": user.username,"token_type":'bearer'}
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="La contraseña no es correcta")
 
-    
-@app.get("/users/me")## ve a decir cual es mi usuario
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@router.get("/users/me")
 async def me(user: User = Depends(current_user)):
     return user
-    
--- Backend/FastAPI/routers/jwt_auth_users.py
+++ Backend/FastAPI/routers/jwt_auth_users.py
@@ -1,99 +1,112 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+## Debemos usar e instalar python-jose[cryptography]
+## passlib[bcrypt]
+
+
+### Users API con autorización OAuth2 JWT ###
+
+from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import jwt, JWTError
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 
-## Debemos usar e instalar python-jose[cryptography]
-## passlib[bcrypt]
-
-
 ALGORITHM = "HS256"
-ACCES_TOKEN_DURATION = 1
-## Se genera el secret así: openssl rand -hex 22
-SECRET = "7d912e0a15a7e663e526a90167119f193a9348d291c2"
+ACCESS_TOKEN_DURATION = 1
+SECRET = "201d573bd7d1344d3a3bfce1550b69102fd11be3db6d379508b6cccc58ea230b"
 
-app = FastAPI()
-oauth2 = OAuth2PasswordBearer(tokenUrl='login') ## esta autenticación es más simple que la de jwt
-# vamos a seguir trabajando con autenticación por contraseña pero ahora encriptación.
+router = APIRouter()
+
+oauth2 = OAuth2PasswordBearer(tokenUrl="login")
 
 crypt = CryptContext(schemes=["bcrypt"])
+
 
 class User(BaseModel):
     username: str
     full_name: str
     email: str
-    disable: bool
+    disabled: bool
+
 
 class UserDB(User):
     password: str
 
+
 users_db = {
     "mouredev": {
         "username": "mouredev",
-        "full_name": "Brais_moure",
+        "full_name": "Brais Moure",
         "email": "braismoure@mourede.com",
-        "disable": False,
-        "password": "$2a$12$WavWn//F9YjzvZiZrBS8ROhva7Rh9jGTsWAb/tvEaXjCqVNJYUoFO"
+        "disabled": False,
+        "password": "$2a$12$B2Gq.Dps1WYf2t57eiIKjO4DXC3IUMUXISJF62bSRiFfqMdOI2Xa6"
     },
-    "moredev2": {
+    "mouredev2": {
         "username": "mouredev2",
-        "full_name": "Brais_moure",
-        "email": "braismoure@mourede.com",
-        "disable": True,
-        "password": "$2a$12$WavWn//F9YjzvZiZrBS8ROhva7Rh9jGTsWAb/tvEaXjCqVNJYUoFO"
+        "full_name": "Brais Moure 2",
+        "email": "braismoure2@mourede.com",
+        "disabled": True,
+        "password": "$2a$12$SduE7dE.i3/ygwd0Kol8bOFvEABaoOOlC8JsCSr6wpwB4zl5STU4S"
     }
 }
 
-def search_userdb(username: str):
+
+def search_user_db(username: str):
     if username in users_db:
-        return(User(**users_db[username]))
+        return UserDB(**users_db[username])
+
+
+def search_user(username: str):
+    if username in users_db:
+        return User(**users_db[username])
+
 
 async def auth_user(token: str = Depends(oauth2)):
-    
-    exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail="Credenciales de atenticacion invalidas", 
-                            headers={"www-Authenticate": "Bearer"})
+
+    exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Credenciales de autenticación inválidas",
+        headers={"WWW-Authenticate": "Bearer"})
 
     try:
-        username = jwt.decode(token,SECRET,algorithms=[ALGORITHM]).get("sub")
+        username = jwt.decode(token, SECRET, algorithms=[ALGORITHM]).get("sub")
         if username is None:
             raise exception
-  
+
     except JWTError:
         raise exception
-    
 
-    return search_userdb(username)
+    return search_user(username)
 
 
-async def current_user(user: User = Depends(auth_user)):    
-    if user.disable:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail="Usuario inactivo", 
-                            )
+async def current_user(user: User = Depends(auth_user)):
+    if user.disabled:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Usuario inactivo")
 
     return user
-@app.post('/login')
-async def login(form: OAuth2PasswordRequestForm = Depends()): # depends dice que recibe datos pero no depende de nadie
+
+
+@router.post("/login")
+async def login(form: OAuth2PasswordRequestForm = Depends()):
     user_db = users_db.get(form.username)
     if not user_db:
-        raise HTTPException(status_code=400,detail="El usuario no es correcto")
-    
-    user = search_userdb(form.username)
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="El usuario no es correcto")
 
-    if not crypt.verify(form.password,user.password):
-        raise HTTPException(status_code=400,detail="La contraseña no es correcta")
-    
-    expire = datetime.utcnow() + timedelta(minutes=ACCES_TOKEN_DURATION)
+    user = search_user_db(form.username)
 
-    access_token = {"sub":user.username,
-                    "exp":expire}
+    if not crypt.verify(form.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="La contraseña no es correcta")
 
-    return {"access_token": jwt.encode(access_token,SECRET, algorithm=ALGORITHM),"token_type":'bearer'}
+    access_token = {"sub": user.username,
+                    "exp": datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_DURATION)}
+
+    return {"access_token": jwt.encode(access_token, SECRET, algorithm=ALGORITHM), "token_type": "bearer"}
 
 
-@app.get("/users/me")## ve a decir cual es mi usuario
+@router.get("/users/me")
 async def me(user: User = Depends(current_user)):
     return user
